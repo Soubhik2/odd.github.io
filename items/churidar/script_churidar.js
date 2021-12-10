05e9e7a0cd1f3b229f4579444d4cdad2d2697a8c
Soubhik2 <95355697+Soubhik2@users.noreply.github.com>
2021-12-10 22:55:52 +0530

    Add files via upload

---
+++ items/churidar/script_churidar.js
@@ -0,0 +1,107 @@
+function button1(){
+	window.location.href = "web/web_side.html";
+	let Switch = 1;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button2(){
+	window.location.href = "web/web_side.html";
+	let Switch = 2;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button3(){
+	window.location.href = "web/web_side.html";
+	let Switch = 3;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button4(){
+	window.location.href = "web/web_side.html";
+	let Switch = 4;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button5(){
+	window.location.href = "web/web_side.html";
+	let Switch = 5;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button6(){
+	window.location.href = "web/web_side.html";
+	let Switch = 6;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button7(){
+	window.location.href = "web/web_side.html";
+	let Switch = 7;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button8(){
+	window.location.href = "web/web_side.html";
+	let Switch = 8;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button9(){
+	window.location.href = "web/web_side.html";
+	let Switch = 9;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button10(){
+	window.location.href = "web/web_side.html";
+	let Switch = 10;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button11(){
+	window.location.href = "web/web_side.html";
+	let Switch = 11;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button12(){
+	window.location.href = "web/web_side.html";
+	let Switch = 12;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button13(){
+	window.location.href = "web/web_side.html";
+	let Switch = 13;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button14(){
+	window.location.href = "web/web_side.html";
+	let Switch = 14;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button15(){
+	window.location.href = "web/web_side.html";
+	let Switch = 15;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button16(){
+	window.location.href = "web/web_side.html";
+	let Switch = 16;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button17(){
+	window.location.href = "web/web_side.html";
+	let Switch = 17;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button18(){
+	window.location.href = "web/web_side.html";
+	let Switch = 18;
+	localStorage.setItem("Switch",Switch);
+}
