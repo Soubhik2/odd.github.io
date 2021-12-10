92a768d4b88f43e42456e4caa661268c6b5bf5cf
Soubhik2 <95355697+Soubhik2@users.noreply.github.com>
2021-12-10 22:58:03 +0530

    Add files via upload

---
+++ items/churidar/web/script_web.js
@@ -0,0 +1,295 @@
+let Switch = localStorage.getItem("Switch");
+
+switch(Switch){
+	case "1": p1();
+		break;
+		
+	case "2": p2();
+		break;
+		
+	case "3": p3(); 
+		break;
+		
+	case "4": p4();
+		break;
+		
+	case "5": p5();
+		break;
+		
+	case "6": p6(); 
+		break;
+		
+	case "7": p7();
+		break;
+		
+	case "8": p8();
+		break;
+		
+	case "9": p9(); 
+		break;
+		
+	case "10": p10();
+		break;
+		
+	case "11": p11();
+		break;
+		
+	case "12": p12(); 
+		break;
+		
+	case "13": p13();
+		break;
+		
+	case "14": p14();
+		break;
+		
+	case "15": p15(); 
+		break;
+	case "16": p16();
+		break;
+		
+	case "17": p17();
+		break;
+		
+	case "18": p18(); 
+		break;
+}
+
+function p1(){
+	document.getElementById("Img-1").src = "Churidar/p1.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 1490 ";
+	document.getElementById("text2").innerHTML = "Sharee 1";
+}
+
+function p2(){
+	document.getElementById("Img-1").src = "Churidar/p2.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 2";	
+}
+
+function p3(){
+	document.getElementById("Img-1").src = "Churidar/p3.jpg"; 
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 3";
+}
+
+function p4(){
+	document.getElementById("Img-1").src = "Churidar/p4.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 1490 ";
+	document.getElementById("text2").innerHTML = "Sharee 1";
+}
+
+function p5(){
+	document.getElementById("Img-1").src = "Churidar/p5.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 2";	
+}
+
+function p6(){
+	document.getElementById("Img-1").src = "Churidar/p6.jpg"; 
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 3";
+}
+
+function p7(){
+	document.getElementById("Img-1").src = "Churidar/p7.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 1490 ";
+	document.getElementById("text2").innerHTML = "Sharee 1";
+}
+
+function p8(){
+	document.getElementById("Img-1").src = "Churidar/p8.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 2";	
+}
+
+function p9(){
+	document.getElementById("Img-1").src = "Churidar/p9.jpg"; 
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 3";
+}
+
+function p10(){
+	document.getElementById("Img-1").src = "Churidar/p10.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 1490 ";
+	document.getElementById("text2").innerHTML = "Sharee 1";
+}
+
+function p11(){
+	document.getElementById("Img-1").src = "Churidar/p11.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 2";	
+}
+
+function p12(){
+	document.getElementById("Img-1").src = "Churidar/p12.jpg"; 
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 3";
+}
+
+function p13(){
+	document.getElementById("Img-1").src = "Churidar/p13.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 1490 ";
+	document.getElementById("text2").innerHTML = "Sharee 1";
+}
+
+function p14(){
+	document.getElementById("Img-1").src = "Churidar/p14.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 2";	
+}
+
+function p15(){
+	document.getElementById("Img-1").src = "Churidar/p15.jpg"; 
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 3";
+}
+
+function p16(){
+	document.getElementById("Img-1").src = "Churidar/p16.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 1490 ";
+	document.getElementById("text2").innerHTML = "Sharee 1";
+}
+
+function p17(){
+	document.getElementById("Img-1").src = "Churidar/p17.jpg";
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 2";	
+}
+
+function p18(){
+	document.getElementById("Img-1").src = "Churidar/p18.jpg"; 
+	document.getElementById("text1").innerHTML = "price Rs:- 870 ";
+	document.getElementById("text2").innerHTML = "Sharee 3";
+}
+
+
+function buttonF(){
+	switch(Switch){
+		case "1": b1();
+			break;
+		
+		case "2": b2();
+			break;
+		
+		case "3": b3(); 
+			break;
+		
+		case "4": b4();
+			break;
+		
+		case "5": b5();
+			break;
+		
+		case "6": b6(); 
+			break;
+		
+		case "7": b7();
+			break;
+			
+		case "8": b8();
+			break;
+		
+		case "9": b9(); 
+			break;
+		
+		case "10": b10();
+			break;
+		
+		case "11": b11();
+			break;
+		
+		case "12": b12(); 
+			break;
+		
+		case "13": b13();
+			break;
+		
+		case "14": b14();
+			break;
+		
+		case "15": b15(); 
+			break;
+			
+		case "16": b16();
+			break;
+		
+		case "17": b17();
+			break;
+		
+		case "18": b18(); 
+			break;
+	}
+}
+
+function b1(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1jLnxs2n5UkWQez1Lm96DkOYPchMVsZId/view?usp=sharing";
+}
+
+function b2(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/13E0GjOHWGUTaLSIuGdpBPzBn7xMTEByW/view?usp=sharing";
+}
+
+function b3(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1d6oxEkFmArWp1zJe8F5sweJBySRWCTdU/view?usp=sharing";
+}
+
+function b4(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1Fh3cYpf-_Dt4KkobQQPx02u7sqADeqUD/view?usp=sharing";
+}
+
+function b5(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1wfFEVfqnRVrwjlOGkAjSHJN6GkALF8J_/view?usp=sharing";	
+}
+
+function b6(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1jF83NgE141DrjWnvbA7SUdvJR5t-X8-m/view?usp=sharing";
+}
+
+function b7(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1Dsl6hUQvs23aoa9xTTHAXhLUhGGZZN45/view?usp=sharing";
+}
+
+function b8(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1Of_CTsogH5iNbFbYRuk9gVdJx7BsetiH/view?usp=sharing";	
+}
+
+function b9(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1X1dTdvWZApdl5fqVzSMzV2A_H0w45J8G/view?usp=sharing";
+}
+
+function b10(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1wd-1nBDAAVysDeO3vYjZ-KcGd4cASRe-/view?usp=sharing";
+}
+
+function b11(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1Ua9JzD1iDMCF1-YIQEepE6tvWv9cueUJ/view?usp=sharing";	
+}
+
+function b12(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/17x7xQy3aeJyBFr9PZi71FNeH9Sg-f00P/view?usp=sharing";
+}
+
+function b13(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1HwuAplg9Jn-TLOMU7usdB902DbleOH98/view?usp=sharing";
+}
+
+function b14(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1WylxM3MzsK3cd8iy4ZhI6wsv_fXB6-jl/view?usp=sharing";	
+}
+
+function b15(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1oVWY9zoYCQpM3KpjZ22unwOFDJOZZi36/view?usp=sharing";
+}
+
+function b16(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1PHE5maWKf5P6udH9Voj6qyrl0ExUCC6W/view?usp=sharing";
+}
+
+function b17(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1VQYu6x8bv0yNDDKQlRoCz_qSVyV1n3-k/view?usp=sharing";	
+}
+
+function b18(){
+	window.location.href = "https://wa.me/919123816216/?text=https://drive.google.com/file/d/1YlcQQW0sbtufiYkj944FpHqyaQEHULcY/view?usp=sharing";
+}
