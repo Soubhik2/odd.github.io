92c64880a2e75f605b388fd79bcab21642c6ae5b
Soubhik2 <95355697+Soubhik2@users.noreply.github.com>
2021-12-10 22:52:57 +0530

    Add files via upload

---
+++ items/Shirts/script_Shirts.js
@@ -0,0 +1,17 @@
+function button1(){
+	window.location.href = "web/web_side.html";
+	let Switch = 1;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button2(){
+	window.location.href = "web/web_side.html";
+	let Switch = 2;
+	localStorage.setItem("Switch",Switch);
+}
+
+function button3(){
+	window.location.href = "web/web_side.html";
+	let Switch = 3;
+	localStorage.setItem("Switch",Switch);
+}
